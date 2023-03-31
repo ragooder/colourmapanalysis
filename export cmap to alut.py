69da8333dc56f3aa601ee1988d7db5559e0f7c1a
Rob G <91266425+ragooder@users.noreply.github.com>
2023-03-31 14:55:03 +0100

    Create export cmap to alut.py

---
+++ export cmap to alut.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def export_cmap(cmap):
+    """
+    Function to export matplotlib colourmap object to a .alut file which can be read by Petrel
+    Input: matplotlib.colors.ListedColormap or matplotlib.colors.LinearSegmentedColormap
+    Output: .alut file (a csv text file with 256 lines of R,G,B,A values)
+    """
+    if cmap.N == 256:
+        arr = (np.array([(cmap(i)) for i in range(256)]) * 255).round().astype(int)
+        np.savetxt(f"{cmap.name}.alut", arr, delimiter=",", fmt='%.f')
+        print(f"{cmap.name}.alut saved")
+    else:
+        # to do: map cmap.N to 256 and export, for now just print message
+        print(f"{cmap.name} has {cmap.N} values, not 256, so not exported")
+    
+    return None
